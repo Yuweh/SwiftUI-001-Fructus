59c64dfffc275c8c8bbfc556698ab71f9a31de35
yuweh <fabergonia@multisyscorp.com>
2023-01-16 01:45:18 +0800

    Onboarding Screen created using the new Page Tab View

---
+++ Fructus/View/StartButtonView.swift
@@ -0,0 +1,43 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by MYS on 1/16/23.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    
+    // MARK: - PROPERTIES
+    // MARK: - BODY
+    
+    var body: some View {
+        Button {
+            print("Exit the onboarding")
+        } label: {
+            HStack(spacing: 9) {
+                Text("Start")
+                
+                Image(systemName: "arrow.right.circle")
+            } //HStack
+            .imageScale(.large)
+            .padding(.horizontal, 16)
+            .padding(.vertical, 10)
+            .background(
+                Capsule()
+                    .strokeBorder(Color.white, lineWidth: 1.25)
+                )
+        } //Button
+        .accentColor(Color.white)
+        
+    } //View
+}
+
+// MARK: - PREVIEW
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .previewLayout(.sizeThatFits)
+    }
+}
