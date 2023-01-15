48ea653337c13e3764e760163f950de1358c628d
fjbMultisys <fabergonia@multisyscorp.com>
2023-01-15 18:37:45 +0800

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by MYS on 1/15/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
